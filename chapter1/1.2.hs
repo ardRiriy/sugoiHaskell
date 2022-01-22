2d9e7db0e7670b17d47d7924e85bb61af21c22bd
ardRiriy <ardririy@protonmail.com>
2022-01-22 08:34:30 +0000

    chapter1

---
+++ chapter1/1.2.hs
@@ -0,0 +1,9 @@
+doubleMe x = x * 2
+
+doubleUs x y = doubleMe x + doubleMe y
+
+
+doubleSmallNumber x =
+    if x > 100
+        then x
+        else doubleMe x
---
+++ chapter1/1.5.hs
@@ -0,0 +1,17 @@
+--10以上のすべての奇数をBANG!に,10以下のすべての奇数をBOOM!に置き換える
+boombang xs =
+    [if x < 10 
+        then "BOOM!" 
+        else "BANG!"| 
+     x <- xs ,
+     odd x] --oddは奇数に対してTrueを返す関数
+
+
+--length関数の独自実装
+--[1 | _ <- xs]はリストxsに対してすべての要素を1に置換したリスト
+length' xs = sum [1 | _ <- xs]
+
+
+-- c `elem` ['A'..'Z']はリストcに含まれる要素が['A'..'Z']のみであるという術語
+    --(['A'..'Z']に含まれない小文字,その他はelem関数によりFalseが返される)
+removeNoUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
